a53c5dbe8328fc9c2cb167a4c0a7cd483eca4d03
Dimitri Marion <dimitri.marion@gmail.com>
2019-08-30 14:22:51 +0200

    add button to choose freq

--- main.js
+++ main.js
@@ -2,18 +2,29 @@
 
 const MAX_FREQ = 5000;
 const STEP = 100;
+const NB_GUESS = 3;
+
+function random(bound) {
+    return Math.floor(Math.random()*bound);
+}
 
 // Sequence of frequencies from 100Hz to 5000Hz: [100, 200, ... , 4900, 5000] 
 const freqRange = Array.from(Array(MAX_FREQ/STEP +1).keys()).map(x => x*STEP).slice(1);
+console.log(freqRange);
+
+// Create an array with 3 freq choosen randomly
+const freqToGuess = Array.from({length: NB_GUESS}, x => freqRange[random(freqRange.length)]);
+console.log(freqToGuess);
+
+// Get the freq to guess
+const freqValue = freqToGuess[random(freqToGuess.length)];
+console.log(freqValue);
 
 // create web audio api context
 const audioCtx = new AudioContext();
 
 // create Oscillator node
 const oscillator = audioCtx.createOscillator();
-
-const freqValue = freqRange[Math.floor(Math.random()*freqRange.length)];
-console.log(freqValue);
 
 oscillator.type = 'sine';
 oscillator.frequency.setValueAtTime(freqValue, audioCtx.currentTime); // value in hertz
@@ -30,3 +41,9 @@ const buttonStop = document.querySelector('.stop');
 buttonStop.addEventListener('click', function() {
     oscillator.disconnect(audioCtx.destination);
 }, false);
+
+const guessButtons = document.querySelectorAll(".guess");
+
+for (let button of guessButtons.entries()) {
+    button[1].textContent = freqToGuess[button[0]];
+}
