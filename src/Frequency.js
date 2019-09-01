6a8ce475928ef3db387b465a9d870a8b96992f83
Dimitri Marion <dimitri.marion@gmail.com>
2019-09-01 16:46:07 +0200

    use a set to avoid duplicate freq

--- src/Frequency.js
+++ src/Frequency.js
@@ -10,8 +10,15 @@ Frequency.prototype.fillFreqRange = function(maxFreq, step) {
     this.freqRange = Array.from(Array(maxFreq/step +1).keys()).map(x => x*step).slice(1);
 }
 
+// Use a set to avoid duplicate freq
 Frequency.prototype.fillFreqToGuess = function(nbGuess) {
-    this.freqToGuess = Array.from({length: nbGuess}, x => this.freqRange[random(this.freqRange.length)]);
+    let freqSet = new Set();
+    
+    while (freqSet.size != nbGuess) {
+        freqSet.add(this.freqRange[random(this.freqRange.length)]);
+    }
+
+    this.freqToGuess = [...freqSet];
 }
 
 Frequency.prototype.setFreqValue = function() {
